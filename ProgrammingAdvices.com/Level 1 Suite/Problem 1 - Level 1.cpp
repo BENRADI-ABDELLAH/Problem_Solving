0c16d07ea420ef6238acd7c1fddbb3694bc7f18b
BENRADI-ABDELLAH <abdellah.benradi@gmail.com>
2024-11-10 03:54:21 +0100

    Ajout de nouveaux fichiers

---
+++ ProgrammingAdvices.com/Level 1 Suite/Problem 1 - Level 1.cpp
@@ -0,0 +1,26 @@
+
+/*
+        Problem 1:
+
+        Write a program to print your name on screen.
+
+*/
+
+
+#include <iostream>
+#include<string>
+using namespace std;
+
+void PrintName(string name) {
+
+    cout << "\n Your name is:" << name << endl;
+
+}
+
+int main()
+{
+
+    PrintName("Benradi abdellah");
+    return 0;
+
+}
