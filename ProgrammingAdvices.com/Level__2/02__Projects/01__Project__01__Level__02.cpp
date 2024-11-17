b929989fb83e1f93a100b06ba12a553dbbd8c355
BENRADI-ABDELLAH <abdellah.benradi@gmail.com>
2024-11-17 15:08:51 +0100

    Ajout de nouveaux fichiers

--- ProgrammingAdvices.com/Level__2/02__Projects/01__Project__01__Level__02.cpp
+++ ProgrammingAdvices.com/Level__2/02__Projects/01__Project__01__Level__02.cpp
@@ -185,3 +185,44 @@ string  Tabs(short NumberOfTabs) {
 
 }
 
+void ShowGameOverScreen() {
+
+	cout << Tabs(2) << "__________________________________________________________\n\n";
+	cout << Tabs(2) << "                 +++ G a m e  O v e r +++\n";
+	cout << Tabs(2) << "__________________________________________________________\n\n";
+
+}
+
+void ShowFinalGameResults(stGameResults GameResults) {
+
+	cout << Tabs(2) << "_____________________ [Game Results ]_____________________\n\n";
+	cout << Tabs(2) << "Game Rounds        : " << GameResults.GameRounds << endl;
+	cout << Tabs(2) << "Player1 won times  : " << GameResults.Player1WinTimes << endl;
+	cout << Tabs(2) << "Computer won times : " << GameResults.Computer2WinTimes << endl;
+	cout << Tabs(2) << "Draw times         : " << GameResults.DrawTimes << endl;
+	cout << Tabs(2) << "Final Winner       : " << GameResults.WinnerName << endl;
+	cout << Tabs(2) << "___________________________________________________________\n";
+
+	SetWinnerScreenColor(GameResults.GameWinner);
+
+}
+short ReadHowManyRounds() {
+
+	short GameRounds = 1;
+
+	do {
+
+		cout << "How Many Rounds 1 to 10 ? \n";
+		cin >> GameRounds;
+
+	} while (GameRounds < 1 || GameRounds >10);
+
+	return GameRounds;
+}
+void ResetScreen() {
+
+	system("cls");
+	system("color 0F");
+
+}
+
