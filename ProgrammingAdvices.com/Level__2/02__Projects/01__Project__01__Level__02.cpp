69c7c5870bdeac4aeb005c32b7229f4ee7b735b9
BENRADI-ABDELLAH <abdellah.benradi@gmail.com>
2024-11-17 15:05:31 +0100

    Ajout de nouveaux fichiers

--- ProgrammingAdvices.com/Level__2/02__Projects/01__Project__01__Level__02.cpp
+++ ProgrammingAdvices.com/Level__2/02__Projects/01__Project__01__Level__02.cpp
@@ -24,3 +24,43 @@ struct stGameResults {
 	string WinnerName = "";
 
 };
+int RandomNumber(int From, int To) {
+	//Function to generate a random number 
+	int randNum = rand() % (To - From + 1) + From;
+
+	return randNum;
+}
+string WinnerName(enWinner Winner) {
+
+	string arrWinnerName[3] = { "Player1","Computer","No Winner" };
+
+	return arrWinnerName[Winner - 1];
+
+}
+enWinner WhoWonTheRound(stRoundInfo RoundInfo) {
+
+	if (RoundInfo.Player1Choice == RoundInfo.ComputerChoice) {
+		return enWinner::Draw;
+	}
+
+	switch (RoundInfo.Player1Choice) {
+	case enGameChoice::Stone:
+		if (RoundInfo.ComputerChoice == enGameChoice::Paper) {
+			return enWinner::Computer;
+		}
+		break;
+	case enGameChoice::Paper:
+		if (RoundInfo.ComputerChoice == enGameChoice::Scissors) {
+			return enWinner::Computer;
+		}
+		break;
+	case enGameChoice::Scissors:
+		if (RoundInfo.ComputerChoice == enGameChoice::Stone) {
+			return enWinner::Computer;
+		}
+		break;
+	}
+
+	return enWinner::Player1;
+}
+
